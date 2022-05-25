101135e755ba191464b5be22a525489163309101
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-25 22:31:14 +0200

    refs

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries'; 
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
